5ff9c96dfee30d20dc5ce753fcc919aee3386af5
Likhitha-Duggi <111375986+Likhitha-Duggi@users.noreply.github.com>
2024-09-11 11:17:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,9 @@ import requests
 # # st.text(fruityvice_response.json())
 # fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-if ingredients_list:
+if ingredient_list:
     ingredients_string = ''
-    for fruit_chosen in ingredients_list:
+    for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width=True)
